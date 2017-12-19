5aeb18aa414f98ee24a20e573d97eaf459f66d80
Matt Walker <mttwlkr@gmail.com>
2017-12-18 17:29:37 -0700

    Add append fetch funtionality to Javascript and Local Storage

--- js/script.js
+++ js/script.js
@@ -1,20 +1,19 @@
-// window.onload = fetchIdea();
+fetchIdea();
 
 var $saveButton = $('#button-save');
 var $ideasList = $('.ideas-container');
 var $grade = $('#grade');
 var arr = ['swill', 'plausible', 'genius'];
 
-
-$('ul').on('click', '.button-delete', deleteIdea);
-
+// $('ul').eq(i).on('click', '.button-delete', deleteIdea);
+// Event.target.parent node.id. store it as a variable. 
+// console.log(idea.uniqueId);
 
 function Idea(titleInput, bodyInput){
   this.titleInput = titleInput;
   this.bodyInput = bodyInput;
   this.quality = 'swill';
-  this.uniqueId = (new Date).getTime()
-  // datestamp as instance variable or key:value
+  this.uniqueId = (new Date).getTime();
 }
 
 $saveButton.click(function(event) {
@@ -23,83 +22,51 @@ $saveButton.click(function(event) {
   var bodyInput = $('#input-body').val();
   var idea = new Idea (titleInput, bodyInput);
   var uniqueId = idea.uniqueId;
-
-  // console.log(uniqueId);
-  // console.log(idea);
+  console.log(idea);
   var stringifiedIdea = JSON.stringify(idea);
-
   localStorage.setItem(uniqueId, stringifiedIdea);
-  console.log(stringifiedIdea)
-  // fetchIdea(uniqueId, idea);
-
-  // $ideasList.prepend(`<li class="card">
-  //   <button class='button-delete circle-buttons'></button>
-  //   <h2 class="ideas-title">${idea.titleInputt}</h2>
-  //   <p class="ideas-content">${idea.bodyInput}</p>
-  //   <button class='button-upvote circle-buttons'></button>
-  //   <button class='button-downvote circle-buttons'></button>
-  //   <h3 class="rating">quality:<span id="grade">${idea.quality}</span></h3>
-  //   </li>`);
-
-  // titleInput.val('');
-  // bodyInput.val('');
-  // $('.button-upvote').on('click', clickUp);
-  // $('.button-downvote').on('click', clickDown);
+  console.log(idea);
+  fetchIdea();
  });
-// function fetchIdea(uniqueId){
-//   var idea = JSON.parse(localStorage.getItem(uniqueId))
-//   // console.log(idea.data(uniqueId));
-//   var displayIdea = $('.ideas-container');
-//   displayIdea.html('');
-//   for(var i=0; i<idea.length; i++) {
-//     var titleIdea = idea[i].title;
-//     var bodyIdea = idea[i].body;
-//     displayIdea.html(`<li class="card">
-//     <button class='button-delete circle-buttons'></button>
-//     <h2 class="ideas-title">${idea.titleInputt}</h2>
-//     <p class="ideas-content">${idea.bodyInput}</p>
-//     <button class='button-upvote circle-buttons'></button>
-//     <button class='button-downvote circle-buttons'></button>
-//     <h3 class="rating">quality:<span id="grade">${idea.quality}</span></h3>
-//     </li>`)
-//   }
-// };
 
-
-function deleteIdea() {
-  $(this).parent('li').remove();
+function fetchIdea(){
+  for (var i=0; i < localStorage.length; i++) {
+    // console.log(localStorage.key(i));
+    var retrieveObject = localStorage.getItem(localStorage.key(i));
+    var idea = JSON.parse(retrieveObject);
+    $('.ideas-container').prepend(`<li class="card" id="${idea.uniqueId}"> 
+    <button class='button-delete circle-buttons'></button>
+    <h2 class="ideas-title">${idea.titleInput}</h2>
+    <p class="ideas-content">${idea.bodyInput}</p>
+    <button class='button-upvote circle-buttons'></button>
+    <button class='button-downvote circle-buttons'></button>
+    <h3 class="rating">quality:<span id="grade">${idea.quality}</span></h3>
+    </li>`);
+    console.log(idea.uniqueId);
+  };
 };
 
-// function clickUp(){
-//   var index = 0;
-//   var rating = "swill";
-//   if (rating = arr[0]) {
-//       index ++;
-//       rating = arr[index];
-//       // console.log(index)
-//       // console.log(rating);
-//     } else if (rating = arr[1]) {
-//       index ++;
-//       rating = arr[index];
-//       console.log(rating);
-//     } else {
-//       rating = arr[2];
-//       console.log(rating);
-//     }
-  // console.log(rating);
-// }
+
+// function deleteIdea() {
+//   var retrieveObject = localStorage.getItem(localStorage.key(i));
+//   var idea = JSON.parse(retrieveObject);
 
 
-function clickDown(){
-  rating = arr[2];
-  if (rating = arr[2] || arr[1]) {
-      index --;
-    } else if (rating = arr[0]){
-      index = arr[0];
-    }
-}
+//   $(this).parent('li').remove();
 
 
+  // for (var i=0; i < localStorage.length; i++) {
+  //   if (idea[i].uniqueId === idea.uniqueId) {
+  //     idea.splice(i, 1);
+  //   }
+  //   console.log(localStorage.getItem(localStorage.key(i)));
+  //}
+  
+  
+// };
+
+  
+//<data-id="${idea.uniqueId}"
 
 
 
